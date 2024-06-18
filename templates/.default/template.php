59d993e7b5a0c095dce663a90fbf59bc9246acdf
Denis Frolov <den-frolov38@yandex.ru>
2024-06-18 16:37:15 +0300

    m

--- templates/.default/template.php
+++ templates/.default/template.php
@@ -3,53 +3,68 @@
 /**
  * @var array $arParams
  * @var array $arResult
+ * @var object $component
  */
 
 use \Bitrix\Main\Localization\Loc;
 
-$messages = Loc::loadLanguageFile(__FILE__);
-if (!defined('SEND_JS')) {
-	\Bitrix\Main\Page\Asset::getInstance()->addJs($this->GetFolder() . '/send.js');
-	define('SEND_JS', 'Y');
+global $APPLICATION;
+if ($arParams['USE_RECAPTCHA'] == 'Y' && $arParams['GOOGLE_RECAPTCHA_KEY'] && $arParams['GOOGLE_RECAPTCHA_SECRET_KEY']) {
+	Bitrix\Main\Page\Asset::getInstance()->addJs('https://www.google.com/recaptcha/api.js?' . $arParams['GOOGLE_RECAPTCHA_KEY']);
 }
+\Bitrix\Main\Page\Asset::getInstance()->addJs($component->getPath() . '/send.js');
+
 ?>
 <? if ($arResult['ITEMS']): ?>
-	<? if ($arResult['POSTED'] != 'Y'): ?>
-		<form action="<?= $APPLICATION->GetCurPage() ?>" method="post" class="df_ajax_form" enctype="multipart/form-data">
-			<input type="hidden" name="IBLOCK_ID" value="<?= $arParams['IBLOCK_ID'] ?>">
-			<? foreach ($arResult['ITEMS'] as $arItem): ?>
-				<div class="form-group">
-					<label for="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>">
-						<span><?= $arItem['NAME'] ?></span>
-						<? if ($arItem['IS_REQUIRED'] == 'Y'): ?>
-							<i style="color:red;">*</i>
-						<? endif; ?>
-					</label>
-					<? if ($arItem['TYPE'] == 'textarea'): ?>
-						<textarea class="form-control" name="<?= $arItem['CODE'] ?>"
-						          id="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>"<?= $arItem['IS_REQUIRED'] == 'Y' ? ' required' : '' ?>></textarea>
-					<? elseif ($arItem['TYPE'] == 'select'): ?>
-						<select
-							name="<?= $arItem['CODE'] ?><?= $arItem['MULTIPLE'] == 'Y' ? '[]' : '' ?>"
-							<?= $arItem['MULTIPLE'] == 'Y' ? ' multiple' : '' ?> class="form-control">
-							<option value="0" selected disabled>Не выбрано</option>
-							<? foreach ($arItem['VALUES'] as $i2 => $arItem2): ?>
-								<option value="<?= $arItem2['ID'] ?>"><?= $arItem2['VALUE'] ?></option>
-							<? endforeach; ?>
-						</select>
-					<? else: ?>
-						<input type="<?= $arItem['TYPE'] ?>" class="form-control"
-						       name="<?= $arItem['CODE'] . ($arItem['MULTIPLE'] == 'Y' ? '[]' : '') ?>"<?= $arItem['MULTIPLE'] == 'Y' ? ' multiple' : '' ?>
-						       id="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>"<?= $arItem['IS_REQUIRED'] == 'Y' ? ' required' : '' ?>>
+	<form action="<?= $APPLICATION->GetCurPage() ?>" method="post" class="df_ajax_form" enctype="multipart/form-data">
+		<input type="hidden" name="IBLOCK_ID" value="<?= $arParams['IBLOCK_ID'] ?>">
+		<?= $arParams['FORM_TITLE'] ?>
+		<? foreach ($arResult['ITEMS'] as $arItem): ?>
+			<div class="form-group">
+				<label for="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>">
+					<span><?= $arItem['NAME'] ?></span>
+					<? if ($arItem['IS_REQUIRED'] == 'Y'): ?>
+						<i style="color:red;">*</i>
 					<? endif; ?>
-				</div>
-			<? endforeach; ?>
-			<? if ($arParams['USE_RECAPTCHA'] == 'Y'): ?>
-				<input type="hidden" name="recaptcha_response" class="recaptcha_response">
-			<? endif; ?>
-			<button type="submit" class="btn btn-primary"><?= $arParams['BUTTON_TEXT'] ?></button>
-		</form>
-	<? else: ?>
-		<div class="df_result"> Сообщение отправлено!</div>
-	<? endif; ?>
+				</label>
+				<? if ($arItem['TYPE'] == 'textarea'): ?>
+					<textarea class="form-control" name="<?= $arItem['CODE'] ?>"
+					          id="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>"<?= $arItem['IS_REQUIRED'] == 'Y' ? ' required' : '' ?>></textarea>
+				<? elseif ($arItem['TYPE'] == 'select'): ?>
+					<select
+						name="<?= $arItem['CODE'] ?><?= $arItem['MULTIPLE'] == 'Y' ? '[]' : '' ?>"
+						<?= $arItem['MULTIPLE'] == 'Y' ? ' multiple' : '' ?> class="form-control">
+						<option value="0" selected disabled>Не выбрано</option>
+						<? foreach ($arItem['VALUES'] as $i2 => $arItem2): ?>
+							<option value="<?= $arItem2['ID'] ?>"><?= $arItem2['VALUE'] ?></option>
+						<? endforeach; ?>
+					</select>
+				<? else: ?>
+					<input type="<?= $arItem['TYPE'] ?>" class="form-control"
+					       name="<?= $arItem['CODE'] . ($arItem['MULTIPLE'] == 'Y' ? '[]' : '') ?>"<?= $arItem['MULTIPLE'] == 'Y' ? ' multiple' : '' ?>
+					       id="<?= $arItem['CODE'] ?>_<?= $arItem['ID'] ?>"<?= $arItem['IS_REQUIRED'] == 'Y' ? ' required' : '' ?>>
+				<? endif; ?>
+			</div>
+		<? endforeach; ?>
+		<? if ($arParams['USE_RECAPTCHA'] == 'Y'): ?>
+			<input type="hidden" name="recaptcha_response" class="recaptcha_response">
+		<? endif; ?>
+		<?= $arParams['AGREE_TEXT'] ?>
+		<button type="submit" class="btn btn-primary"><?= $arParams['BUTTON_TEXT'] ?></button>
+	</form>
 <? endif; ?>
+
+
+<script type="text/javascript">
+	let signedParameters = '<?= $this->getComponent()->getSignedParameters() ?>';
+	<?php if ($arParams['USE_RECAPTCHA'] == 'Y' && $arParams['GOOGLE_RECAPTCHA_KEY'] && $arParams['GOOGLE_RECAPTCHA_SECRET_KEY']): ?>
+	grecaptcha.ready(function () {
+		grecaptcha.execute('<?= $arParams['GOOGLE_RECAPTCHA_KEY'] ?>', {action: 'contact'}).then(function (token) {
+			let recaptchaResponse = document.getElementsByClassName('recaptcha_response');
+			for (let recaptchaResponseElement of recaptchaResponse) {
+				recaptchaResponseElement.value = token
+			}
+		});
+	});
+	<?php endif ?>
+</script>
