26a93ee1747e421767a2910a5570fa563a844984
denis <mail@df7.ru>
2024-07-11 09:38:19 +0300

    1

--- install/components/df/df.send/class.php
+++ install/components/df/df.send/class.php
@@ -257,7 +257,7 @@ class dfForms extends CBitrixComponent implements Controllerable, Errorable
 			} else {
 				if ($arProperty['PROPERTY_TYPE'] == 'L') {
 					$arProperty['TYPE'] = 'select';
-					$property_enums = CIBlockPropertyEnum::GetList(array("SORT" => "ASC", "VALUE" => "ASC"), array("IBLOCK_ID" => $arParams['IBLOCK_ID'], "CODE" => $arProperty['CODE']));
+					$property_enums = CIBlockPropertyEnum::GetList(array("SORT" => "ASC", "ID" => "ASC"), array("IBLOCK_ID" => $arParams['IBLOCK_ID'], "CODE" => $arProperty['CODE']));
 					while ($enum_fields = $property_enums->GetNext()) {
 						$arProperty['VALUES'][] = $enum_fields;
 					}
