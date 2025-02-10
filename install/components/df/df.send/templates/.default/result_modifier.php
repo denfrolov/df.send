be26c95d05b278384af43ea736c9474da0c9d7c4
Denis Frolov <den-frolov38@yandex.ru>
2025-02-10 15:35:02 +0300

    Update result_modifier.php

--- install/components/df/df.send/templates/.default/result_modifier.php
+++ install/components/df/df.send/templates/.default/result_modifier.php
@@ -3,3 +3,9 @@
  * @var array $arParams
  * @var array $arResult
  */
+$fullUrl = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+foreach ($arResult['ITEMS'] as $i => &$arItem) {
+	if (stripos($arItem['CODE'], 'link') !== false) {
+		$arItem['VALUE'] = $fullUrl;
+	}
+}
